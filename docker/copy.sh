bd5cc8bf01416bc7241d353355e92c6da1da9c2a
Yongqi <245938767@qq.com>
2023-01-31 10:43:59 +0800

    update docker

--- docker/copy.sh
+++ docker/copy.sh
@@ -19,23 +19,23 @@ cp -r ../iiss-ui/dist/** ./nginx/html/dist
 
 # copy jar
 echo "begin copy iiss-gateway "
-cp ../iiss-gateway/target/iiss-gateway.jar ./ruoyi/gateway/jar
+cp ../iiss-gateway/target/iiss-gateway.jar ./iiss/gateway/jar
 
 echo "begin copy iiss-auth "
-cp ../iiss-auth/target/iiss-auth.jar ./ruoyi/auth/jar
+cp ../iiss-auth/target/iiss-auth.jar ./iiss/auth/jar
 
 echo "begin copy iiss-visual "
-cp ../iiss-visual/iiss-monitor/target/iiss-visual-monitor.jar  ./ruoyi/visual/monitor/jar
+cp ../iiss-visual/iiss-monitor/target/iiss-visual-monitor.jar  ./iiss/visual/monitor/jar
 
 echo "begin copy iiss-modules-system "
-cp ../iiss-modules/iiss-system/target/iiss-modules-system.jar ./ruoyi/modules/system/jar
+cp ../iiss-modules/iiss-system/target/iiss-modules-system.jar ./iiss/modules/system/jar
 
 echo "begin copy iiss-modules-file "
-cp ../iiss-modules/iiss-file/target/iiss-modules-file.jar ./ruoyi/modules/file/jar
+cp ../iiss-modules/iiss-file/target/iiss-modules-file.jar ./iiss/modules/file/jar
 
 echo "begin copy iiss-modules-job "
-cp ../iiss-modules/iiss-job/target/iiss-modules-job.jar ./ruoyi/modules/job/jar
+cp ../iiss-modules/iiss-job/target/iiss-modules-job.jar ./iiss/modules/job/jar
 
 echo "begin copy iiss-modules-gen "
-cp ../iiss-modules/iiss-gen/target/iiss-modules-gen.jar ./ruoyi/modules/gen/jar
+cp ../iiss-modules/iiss-gen/target/iiss-modules-gen.jar ./iiss/modules/gen/jar
 
