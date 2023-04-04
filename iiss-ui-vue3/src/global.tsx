5482de6167a79736027fff3ca628d6e0e60ca1d1
Yongqi <245938767@qq.com>
2023-04-04 20:37:48 +0800

    添加高德地图

--- iiss-ui-vue3/src/global.tsx
+++ iiss-ui-vue3/src/global.tsx
@@ -1,10 +1,19 @@
 import { Button, message, notification } from 'antd';
 import { useIntl } from 'umi';
 import defaultSettings from '../config/defaultSettings';
+import { config as AmapReactConfig } from '@amap/amap-react';
 
 const { pwa } = defaultSettings;
 const isHttps = document.location.protocol === 'https:';
 
+
+AmapReactConfig.version = '2.0'; // 默认2.0，这里可以不修改
+AmapReactConfig.key = 'ae6d6658a2f94a3d0ed6a1c88ea51453';
+AmapReactConfig.plugins = [
+  'AMap.ToolBar',
+  'AMap.MoveAnimation',
+  // 在此配置你需要预加载的插件，如果不配置，在使用到的时候会自动异步加载
+];
 const clearCache = () => {
   // remove all caches
   if (window.caches) {
--- iiss-ui-vue3/src/pages/warehouse/map/index.tsx
+++ iiss-ui-vue3/src/pages/warehouse/map/index.tsx
@@ -1,12 +1,24 @@
-import WrapContent from "@/components/WrapContent";
+import WrapContent from '@/components/WrapContent';
+// YourComponent.vue
+import { Amap, Marker } from '@amap/amap-react';
+import { useRef } from 'react';
 
-const groupMap: React.FC=()=>{
+const groupMap = () => {
+  // const mapRef = useRef();
 
-return(
-<WrapContent>
-    hello
-</WrapContent>
-
-);
-}
+  return (
+    <>
+     <Amap zoom={15} center={[116.473179, 39.993169]}>
+          <Marker
+            position={[116.473179, 39.993169]}
+            label={{
+              content: "Hello, AMap-React!",
+              direction: "bottom"
+            }}
+            draggable
+          />
+        </Amap>
+    </>
+  );
+};
 export default groupMap;
